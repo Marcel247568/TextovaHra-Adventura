5b82cd0248920e446e49b15684398f8a30ccd517
Marcel <247568@vutbr.cz>
2022-10-19 20:14:20 +0200

    Zadání

---
+++ TextovaHra/TextovaHra.cpp
@@ -0,0 +1,12 @@
+﻿// TextovaHra.cpp : Defines the entry point for the application.
+//
+
+#include "TextovaHra.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
