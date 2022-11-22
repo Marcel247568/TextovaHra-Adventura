cf6eab4a4c36157359b141b03799f6fce3bfac46
Marcel Kubín <marcelkubin2@gmail.com>
2022-11-23 00:24:17 +0100

    Přídání dynamické alokace paměti při načítání mapy

--- TextovaHra/GameLib/GameLib.cpp
+++ TextovaHra/GameLib/GameLib.cpp
@@ -128,14 +128,16 @@ void printMap() {
 	setColor(BACKGROUND, BLACK);
 	setColor(BRIGHT_FOREGROUND, WHITE);
 
-	char line[255];
+	char* line;
+	line = (char*)malloc(255 * sizeof(char));
 	FILE* fmap;
 	if (fopen_s(&fmap, "../../../data/map.dat", "r")) errorMessage("soubor map.dat se nepodarilo otevrit");
 	
 	while (!feof(fmap)) {
-		fscanf_s(fmap, "#%[^\n]\n", line, sizeof(line));
+		fscanf_s(fmap, "#%[^\n]\n", line, 255 * sizeof(char));
 		printf_s("%s\n", line);
 		relativeCursorPosition(COLUMNS / 5 + 1, 0);
 	}
 	fclose(fmap);
+	free(line);
 }
